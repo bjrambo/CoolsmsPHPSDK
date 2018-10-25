9eb3f85a673cbc2b17f235b5f30cf152bdb6b63e
BJRambo <qw5414@naver.com>
2018-10-26 06:35:12 +0900

    그룹추가 부분 정리 및 스케줄 기능 추가

--- src/Coolsms/message/Api/GroupMessage.php
+++ src/Coolsms/message/Api/GroupMessage.php
@@ -32,7 +32,36 @@ class GroupMessage extends Coolsms
 
 	public static function addGroupMessages($groupId, $options)
 	{
-		return self::request('PUT', "messages/v4/groups/{$groupId}/messages", $options);
+		if (is_array($options->to))
+		{
+			$message = array();
+			$i = 0;
+			foreach ($options->to as $key => $phoneNumber)
+			{
+				$message[$key] = new \stdClass();
+				$message[$key]->text = $options->text;
+				$message[$key]->type = $options->type;
+				$message[$key]->to = $phoneNumber;
+				$message[$key]->from = $options->from;
+				$i++;
+			}
+
+			$args = new \stdClass();
+			$args->messages = json_encode($message);
+		}
+		else
+		{
+			$message = new \stdClass();
+			$message->text = $options->text;
+			$message->type = $options->type;
+			$message->to = $options->to;
+			$message->from = $options->from;
+
+			$args = new \stdClass();
+			$args->messages = json_encode(array($message));
+		}
+
+		return self::request('PUT', "messages/v4/groups/{$groupId}/messages", $args);
 	}
 
 	public static function sendGroupMessages($groupId)
@@ -40,13 +69,20 @@ class GroupMessage extends Coolsms
 		return self::request('POST', "messages/v4/groups/{$groupId}/send");
 	}
 
-	public static function scheduleGroupMessage()
+	/**
+	 * @param $groupId
+	 * @param $date string : ISO 8601 형식의 발송할 시간
+	 * @return mixed
+	 */
+	public static function scheduleGroupMessage($groupId, $date)
 	{
-
+		$args = new \stdClass();
+		$args->scheduledDate = $date;
+		return self::request('POST', "messages/v4/groups/{$groupId}/schedule", $args);
 	}
 
-	public static function cancelScheduledGroupMessage()
+	public static function cancelScheduledGroupMessage($groupId)
 	{
-
+		return self::request('DELETE', "messages/v4/groups/{$groupId}/schedule");
 	}
 }
